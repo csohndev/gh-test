1252e6164e9b4d65e4626cbcad78b8a41e3da1eb
Christopher Sohn <chris@csohn.dev>
2025-07-30 18:00:56 -0500

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/gh-test/" : "/",
+  base: "/gh-test/",
   integrations: [
     starlight({
       title: "My Docs",
